0205348e760ff1d083993d8f705e5ecf0a80c8a7
Tatyana <tatyana.dovganskaya@gmail.com>
2022-11-03 13:49:55 +0300

    Показать двумерный массив размером m×n заполненный вещественными числами

---
+++ 001/Program.cs
@@ -0,0 +1,31 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+void PrintArray(int[,] arr)
+{
+  for(int i = 0; i < arr.GetLength(0); i++)
+{
+    for(int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write($"{arr[i,j]}");
+    }
+Console.WriteLine();
+}  
+}
+void FillArray(int[,] arr)
+{
+ for(int i = 0; i < arr.GetLength(0); i++)
+{
+    for(int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i,j] = new Random().Next(1,10);
+    }   
+}
+}
+
+Console.WriteLine("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int[,] array = new int[m,n];
+FillArray(array);
+PrintArray(array);
